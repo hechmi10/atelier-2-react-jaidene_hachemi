9692eddd723394e3945db0b24fdf62f6cce14ddb
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2025-02-11 21:44:36 +0100

    Ajouter des ressources et des fonctions JSX

---
+++ src/Components/Event.jsx
@@ -0,0 +1,14 @@
+
+
+function Event(){
+    return(
+        <div className="card">
+            <div className="row">
+                <div className="col">
+                    <img src="src\assets\placeholder.jpg"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Event

573665759835ff59bf3e7b1d002581cdf8a85e0f
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2025-03-12 08:05:56 +0100

    Update Event

--- src/Components/UpdateEvent.jsx
+++ src/Components/UpdateEvent.jsx
@@ -3,6 +3,7 @@ import { editEvent } from '../services/api';
 import { useParams } from 'react-router-dom';
 import { z } from 'zod';
 import {zodResolver} from '@hookform/resolvers/zod';
+import db from '../api/db.json'
 const eventSchema=z.object({
     name:z.string().min(3,"C'est moins de 3 caractères"),
     description:z.string().min(10,"C'est moins de 10 caractères"),
@@ -13,8 +14,9 @@ const eventSchema=z.object({
 })
 
 function UpdateEvent(){
-    const {id}=useParams()
-    const {register,handleSubmit,reset}=useForm({resolver:zodResolver(eventSchema)});
+    let {id}=useParams()
+    id=db.events.id;
+    const {register,handleSubmit,reset,formState:{errors}}=useForm({resolver:zodResolver(eventSchema)});
     const onSubmit=async (data)=>{
         await editEvent(id,data);
         reset();
@@ -23,14 +25,19 @@ function UpdateEvent(){
         <form className='form-group' onSubmit={handleSubmit(onSubmit)}>
             <label className='form-label'>Name</label><br/>
             <input className='form-control' {...register('name')} placeholder="Event Name" required /><br/>
+            {errors.name && <p>{errors.name.message}</p>}
             <label className='form-label'>Description</label><br/>
             <textarea className='form-control' {...register('description')} placeholder="Description" required /><br/>
+            {errors.description && <p>{errors.description.message}</p>}
             <label className='form-label'>Price</label><br/>
             <input type='number' className='form-control' {...register('price',{valueAsNumber:true})} placeholder="Price" required /><br/>
+            {errors.price && <p>{errors.price.message}</p>}
             <label className='form-label'>Number of Tickets</label><br/>
             <input type='number' className='form-control' {...register('nbTickets',{valueAsNumber:true})} placeholder="Number of Tickets" required /><br/>
+            {errors.nbTickets && <p>{errors.nbTickets.message}</p>}
             <label className='form-label'>Image</label><br/>
             <input type='file' className='form-control' {...register('img')} placeholder="Image" required /><br/>
+            {errors.img && <p>{errors.img.message}</p>}
             <button className='btn btn-primary' type="submit">Ajouter</button><br/>
         </form>
     )
